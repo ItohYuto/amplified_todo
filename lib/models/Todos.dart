8b8841573351d663e2dabda20c8c02b48e26bfac
Yuto <agbt.itohyuto@gmail.com>
2022-09-28 15:55:02 +0900

    バックエンド環境の追加

---
+++ lib/models/Todos.dart
@@ -0,0 +1,188 @@
+/*
+* Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License").
+* You may not use this file except in compliance with the License.
+* A copy of the License is located at
+*
+*  http://aws.amazon.com/apache2.0
+*
+* or in the "license" file accompanying this file. This file is distributed
+* on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+* express or implied. See the License for the specific language governing
+* permissions and limitations under the License.
+*/
+
+// NOTE: This file is generated and may not follow lint rules defined in your app
+// Generated files can be excluded from analysis in analysis_options.yaml
+// For more info, see: https://dart.dev/guides/language/analysis-options#excluding-code-from-analysis
+
+// ignore_for_file: public_member_api_docs, annotate_overrides, dead_code, dead_codepublic_member_api_docs, depend_on_referenced_packages, file_names, library_private_types_in_public_api, no_leading_underscores_for_library_prefixes, no_leading_underscores_for_local_identifiers, non_constant_identifier_names, null_check_on_nullable_type_parameter, prefer_adjacent_string_concatenation, prefer_const_constructors, prefer_if_null_operators, prefer_interpolation_to_compose_strings, slash_for_doc_comments, sort_child_properties_last, unnecessary_const, unnecessary_constructor_name, unnecessary_late, unnecessary_new, unnecessary_null_aware_assignments, unnecessary_nullable_for_final_variable_declarations, unnecessary_string_interpolations, use_build_context_synchronously
+
+import 'package:amplify_core/amplify_core.dart';
+import 'package:flutter/foundation.dart';
+
+/** This is an auto generated class representing the Todos type in your schema. */
+@immutable
+class Todos extends Model {
+  static const classType = const _TodosModelType();
+  final String id;
+  final String name;
+  final String description;
+  final bool isComplete;
+  final TemporalDateTime createdAt;
+  final TemporalDateTime updatedAt;
+
+  @override
+  getInstanceType() => classType;
+
+  @override
+  String getId() {
+    return id;
+  }
+
+  const Todos._internal(
+      {@required this.id,
+      @required this.name,
+      this.description,
+      @required this.isComplete,
+      this.createdAt,
+      this.updatedAt});
+
+  factory Todos(
+      {String id,
+      @required String name,
+      String description,
+      @required bool isComplete}) {
+    return Todos._internal(
+        id: id == null ? UUID.getUUID() : id,
+        name: name,
+        description: description,
+        isComplete: isComplete);
+  }
+
+  bool equals(Object other) {
+    return this == other;
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(other, this)) return true;
+    return other is Todos &&
+        id == other.id &&
+        name == other.name &&
+        description == other.description &&
+        isComplete == other.isComplete;
+  }
+
+  @override
+  int get hashCode => toString().hashCode;
+
+  @override
+  String toString() {
+    var buffer = new StringBuffer();
+
+    buffer.write("Todos {");
+    buffer.write("id=" + "$id" + ", ");
+    buffer.write("name=" + "$name" + ", ");
+    buffer.write("description=" + "$description" + ", ");
+    buffer.write("isComplete=" +
+        (isComplete != null ? isComplete.toString() : "null") +
+        ", ");
+    buffer.write("createdAt=" +
+        (createdAt != null ? createdAt.format() : "null") +
+        ", ");
+    buffer.write(
+        "updatedAt=" + (updatedAt != null ? updatedAt.format() : "null"));
+    buffer.write("}");
+
+    return buffer.toString();
+  }
+
+  Todos copyWith(
+      {String id, String name, String description, bool isComplete}) {
+    return Todos._internal(
+        id: id ?? this.id,
+        name: name ?? this.name,
+        description: description ?? this.description,
+        isComplete: isComplete ?? this.isComplete);
+  }
+
+  Todos.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        name = json['name'],
+        description = json['description'],
+        isComplete = json['isComplete'],
+        createdAt = json['createdAt'] != null
+            ? TemporalDateTime.fromString(json['createdAt'])
+            : null,
+        updatedAt = json['updatedAt'] != null
+            ? TemporalDateTime.fromString(json['updatedAt'])
+            : null;
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'name': name,
+        'description': description,
+        'isComplete': isComplete,
+        'createdAt': createdAt?.format(),
+        'updatedAt': updatedAt?.format()
+      };
+
+  static final QueryField ID = QueryField(fieldName: "id");
+  static final QueryField NAME = QueryField(fieldName: "name");
+  static final QueryField DESCRIPTION = QueryField(fieldName: "description");
+  static final QueryField ISCOMPLETE = QueryField(fieldName: "isComplete");
+  static var schema =
+      Model.defineSchema(define: (ModelSchemaDefinition modelSchemaDefinition) {
+    modelSchemaDefinition.name = "Todos";
+    modelSchemaDefinition.pluralName = "Todos";
+
+    modelSchemaDefinition.authRules = [
+      AuthRule(authStrategy: AuthStrategy.PUBLIC, operations: [
+        ModelOperation.CREATE,
+        ModelOperation.UPDATE,
+        ModelOperation.DELETE,
+        ModelOperation.READ
+      ])
+    ];
+
+    modelSchemaDefinition.addField(ModelFieldDefinition.id());
+
+    modelSchemaDefinition.addField(ModelFieldDefinition.field(
+        key: Todos.NAME,
+        isRequired: true,
+        ofType: ModelFieldType(ModelFieldTypeEnum.string)));
+
+    modelSchemaDefinition.addField(ModelFieldDefinition.field(
+        key: Todos.DESCRIPTION,
+        isRequired: false,
+        ofType: ModelFieldType(ModelFieldTypeEnum.string)));
+
+    modelSchemaDefinition.addField(ModelFieldDefinition.field(
+        key: Todos.ISCOMPLETE,
+        isRequired: true,
+        ofType: ModelFieldType(ModelFieldTypeEnum.bool)));
+
+    modelSchemaDefinition.addField(ModelFieldDefinition.nonQueryField(
+        fieldName: 'createdAt',
+        isRequired: false,
+        isReadOnly: true,
+        ofType: ModelFieldType(ModelFieldTypeEnum.dateTime)));
+
+    modelSchemaDefinition.addField(ModelFieldDefinition.nonQueryField(
+        fieldName: 'updatedAt',
+        isRequired: false,
+        isReadOnly: true,
+        ofType: ModelFieldType(ModelFieldTypeEnum.dateTime)));
+  });
+}
+
+class _TodosModelType extends ModelType<Todos> {
+  const _TodosModelType();
+
+  @override
+  Todos fromJson(Map<String, dynamic> jsonData) {
+    return Todos.fromJson(jsonData);
+  }
+}
